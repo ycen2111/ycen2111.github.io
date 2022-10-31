3e83f3bb5029a5feb75be25d3bf7b5e0660bb15a
Yang Cen <ycen2111@gmail.com>
2022-10-31 12:14:17 +0000

    backup

--- Backup.sh
+++ Backup.sh
@@ -1,8 +1,20 @@
-cd D://alog
-if [$? -eq 0]; then
-echo "Cannot found folder D://Blog"
+cd D://Blog
+
+if [ $? -ne 0 ]; then
 read -n 1
 exit
 fi
+
 cp -a .//Hexo//source .//source//ycen2111.github.io
-git remote rm origin && git remote add origin https://github.com/ycen2111/ycen2111.github.io.git && git add . && git commit -m "backup" && git push origin source
+cp -a .//Hexo//themes .//source//ycen2111.github.io
+cp -a .//Hexo//_config.yml .//source//ycen2111.github.io
+cp -a .//Hexo//package.json .//source//ycen2111.github.io
+cd .//source//ycen2111.github.io && git remote rm origin && git remote add origin https://github.com/ycen2111/ycen2111.github.io.git && git add . && git commit -m "backup" && git push origin source
+
+if [ $? -ne 0 ]; then
+read -n 1
+exit
+fi
+
+echo "Done"
+sleep 1
