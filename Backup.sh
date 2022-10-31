d8bc7699289fd449ea6d5a0f008f6a29cc52e629
Yang Cen <ycen2111@gmail.com>
2022-10-31 14:13:14 +0000

    backup

--- Backup.sh
+++ Backup.sh
@@ -6,10 +6,23 @@ exit
 fi
 
 cp -a .//Hexo//source .//source//ycen2111.github.io
-cp -a .//Hexo//themes//next//_config.yml .//source//ycen2111.github.io//themes//next
-cp -a .//Hexo//themes//next//package.json .//source//ycen2111.github.io//themes//next
 cp -a .//Hexo//_config.yml .//source//ycen2111.github.io
 cp -a .//Hexo//package.json .//source//ycen2111.github.io
+
+if [ ! -d ".//source//ycen2111.github.io//themes//next" ]; then
+echo "Create .//source//ycen2111.github.io//themes//next"
+mkdir -p .//source//ycen2111.github.io//themes//next
+
+if [ $? -ne 0 ]; then
+read -n 1
+exit
+fi
+
+fi
+
+cp -a .//Hexo//themes//next//_config.yml .//source//ycen2111.github.io//themes//next
+cp -a .//Hexo//themes//next//package.json .//source//ycen2111.github.io//themes//next
+
 cd .//source//ycen2111.github.io && git remote rm origin && git remote add origin https://github.com/ycen2111/ycen2111.github.io.git && git add . && git commit -m "backup" && git push origin source
 
 if [ $? -ne 0 ]; then
