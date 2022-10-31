e567c752b616fbf68c5068f6a2c139c813988081
Yang Cen <ycen2111@gmail.com>
2022-10-31 14:07:01 +0000

    backup

--- Backup.sh
+++ Backup.sh
@@ -6,7 +6,8 @@ exit
 fi
 
 cp -a .//Hexo//source .//source//ycen2111.github.io
-cp -a .//Hexo//themes .//source//ycen2111.github.io
+cp -a .//Hexo//themes//next//_config.yml .//source//ycen2111.github.io//themes//next
+cp -a .//Hexo//themes//next//package.json .//source//ycen2111.github.io//themes//next
 cp -a .//Hexo//_config.yml .//source//ycen2111.github.io
 cp -a .//Hexo//package.json .//source//ycen2111.github.io
 cd .//source//ycen2111.github.io && git remote rm origin && git remote add origin https://github.com/ycen2111/ycen2111.github.io.git && git add . && git commit -m "backup" && git push origin source
@@ -17,4 +18,4 @@ exit
 fi
 
 echo "Done"
-sleep 1
+sleep 5
